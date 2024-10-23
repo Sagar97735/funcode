7999b7f6e388f8979778cb64b9fc7bd9b80b8cff
Sagar7071 <sagars79850@gmail.com>
2024-10-23 07:33:17 +0530

    github data added

--- index.js
+++ index.js
@@ -1,12 +1,28 @@
 console.log("chai aur code");
-const express = require('express')
+const express = require('express');
 const app = express()
-const port = 3000
-
+const port = 3000;
+const data = {
+  "name": "Sagar",
+  "roll_no": "12345", 
+    "street": "123 Main St",
+    "city": "Pune",
+    "state": "Maharashtra",
+    "zip": "411001"}
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello World!');
 })
+app.get('/github',(req,res)=>{
+  res.json(data);
+})
+app.get('/login',(req,res)=>{
+  res.send('u are logged in');
+  })
+  app.get('/profile',(req,res)=>{
+    res.send('my name is sagar');
+  })
+  app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+  })
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+
